d6913c4f56245cbcaf58061d1301c93f9cd7cb30
rusty-belcher <44824894+rusty-belcher@users.noreply.github.com>
2019-04-09 12:38:15 -0400

    added a user-defined function

--- 08_functions.sql
+++ 08_functions.sql
@@ -2,3 +2,31 @@
 Script Date: April 5th ,2019
 Developped by Matthew, Sergei, Levar, Tony
  */
+
+/*********USER DEFINED FUNCTION*********/
+
+/* function employee.getFinesFN returns the status of a library users fines */
+
+-- drop function if exsits
+IF sysobjects(N'employee.FinesFN', N'FN') IS NOT NULL
+    DROP FUNCTION employee.getFinesFN;
+    go
+
+create function employee.getFinesFN
+      (
+        @MemberID as int
+      )
+      return int
+as
+  begin
+    declare @FineOwed as smallmoney
+    select @FineOwed = ELH.FineAssessed
+    from employee.LoanHistory as ELH
+    where ELH.MemberID = @MemberID
+      if (@FineOwed is null)
+        begin
+          set @FineOwed = 0
+        end
+    return @FineOwed
+  end;
+go
