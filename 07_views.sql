99ba253992c0138f8c824dae1f8daa33059567d7
rusty-belcher <44824894+rusty-belcher@users.noreply.github.com>
2019-04-09 12:49:00 -0400

    added 2 views

--- 07_views.sql
+++ 07_views.sql
@@ -2,3 +2,40 @@
 Script Date: April 5th ,2019
 Developped by Matthew, Sergei, Levar, Tony
  */
+
+ /*FacultyView, that return the ID, first and last name of faculty*/
+ if OBJECT_ID('customer.FacultyView', 'V') is not null
+   drop view customer.FacultyView;
+ go
+
+ create view customer.FacultyView
+ as
+   SELECT
+   C.FacultyID as 'Faculty ID',
+   concat_ws(FirstName, LastName) as 'Faculty Full Name'
+   from customer.Faculty as C;
+ go
+
+-- test
+ select *
+ from customer.FacultyView;
+ go
+
+ /*StudentView, that return the ID, Student Number, first and last name of students*/
+ if OBJECT_ID('customer.StudentView', 'V') is not null
+   drop view customer.StudentView;
+ go
+
+ create view customer.StudentView
+ as
+   SELECT
+   C.StudentID as 'Student ID',
+   C.StudentNumber as 'Student Number',
+   concat_ws(FirstName, LastName) as 'Student Full Name'
+   from customer.Students as C;
+ go
+
+-- test
+ select *
+ from customer.StudentView;
+ go
