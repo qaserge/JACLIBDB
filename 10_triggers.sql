2fc2fb2c5afa3aaeb273be6dce3a1cb127504518
rusty-belcher <44824894+rusty-belcher@users.noreply.github.com>
2019-04-10 19:45:20 -0400

    Update 10_triggers.sql

--- 10_triggers.sql
+++ 10_triggers.sql
@@ -2,3 +2,26 @@
 Script Date: April 5th ,2019
 Developped by Matthew, Sergei, Levar, Tony
  */
+/* TRIGGER WHEN NEW LOAN ADDED */
+CREATE TRIGGER trLoanReturned
+ON employee.Loans
+AFTER UPDATE
+AS
+	begin
+		DECLARE @CopyID as int,
+				@ISBN as varchar(13),
+				@ReturnedDate as datetime
+
+		SELECT	@CopyID = CopyID,
+				@ISBN = ItemID,
+				@ReturnedDate = ReturnedDate
+		FROM Inserted
+		if(@ReturnedDate is not null)
+			begin
+				UPDATE Item.Copy
+SET Copy.isLoaned = 0
+WHERE Copy.CopyNo = @CopyID AND Copy.ISBN = @ISBN
+end
+	end
+;
+go
