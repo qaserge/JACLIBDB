18fd9d2d21b63ae64ea52687e39aa4ee6fb32951
rusty-belcher <44824894+rusty-belcher@users.noreply.github.com>
2019-04-10 15:06:51 -0400

    Update 08_functions.sql

--- 08_functions.sql
+++ 08_functions.sql
@@ -2,6 +2,39 @@
 Script Date: April 5th ,2019
 Developped by Matthew, Sergei, Levar, Tony
  */
+ 
+ -- 6. What is the average length of a loan?
+
+if OBJECT_ID('employee.getNumberOfDaysFn', 'Fn') is not null
+drop function employee.getNumberOfDaysFn
+;
+go
+
+
+create function employee.getNumberOfDaysFn
+(
+@OutDate as datetime,
+@DueDate as datetime
+)
+returns int
+as 
+begin
+declare @Period as int
+select @Period = abs(DATEDIFF(DAY, @OutDate, @DueDate ))
+if (@Period is null)
+begin
+set @Period = 0
+end
+return @Period
+end
+;
+go
+
+
+select AVG(employee.getNumberOfDaysFn(OutDate, DueDate)) as 'Average length of a loan'
+from employee.Loans
+;
+go
 
 /*********USER DEFINED FUNCTION*********/
 
