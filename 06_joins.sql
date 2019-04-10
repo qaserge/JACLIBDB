bfe4f582b4004a5f6bc2519beb30c6f4b16d71f5
rusty-belcher <44824894+rusty-belcher@users.noreply.github.com>
2019-04-10 15:06:15 -0400

    Update 06_joins.sql

--- 06_joins.sql
+++ 06_joins.sql
@@ -2,3 +2,13 @@
 Script Date: April 5th ,2019
 Developped by Matthew, Sergei, Levar, Tony
  */
+
+-- 4. What percentage of the books was loaned out at least once last year?
+
+select 
+CAST( (count(distinct LH.ItemID) * 100) / (count(I.ItemID)) AS NUMERIC(10, 2) ) as 'Percentage %'
+from material.Items as I
+left outer join employee.LoanHistory as LH
+on I.ItemID = LH.ItemID
+;
+go
